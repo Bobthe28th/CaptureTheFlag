47860d1117f4deebb47d5b1588da7373d68c473e
Bobthe28th <owenmjones28@gmail.com>
2022-01-18 23:11:47 -0800

    Adds "!"

--- src/me/bobthe28th/capturethefart/Main.java
+++ src/me/bobthe28th/capturethefart/Main.java
@@ -89,7 +89,7 @@ public class Main extends JavaPlugin implements Listener {
         }
 
         for(Player player : Bukkit.getOnlinePlayers()) {
-            player.setPlayerListHeader("capture the FART");
+            player.setPlayerListHeader("capture the FART!");
         }
 
         Bukkit.broadcastMessage("farted");
@@ -99,7 +99,7 @@ public class Main extends JavaPlugin implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
-        player.setPlayerListHeader("capture the FART");
+        player.setPlayerListHeader("capture the FART!");
 
     }
 
