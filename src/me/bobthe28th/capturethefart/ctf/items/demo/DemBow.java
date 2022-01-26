ea4d90b674765e7bee2b38ce862ff31cb5e42de2
Bobthe28th <owenmjones28@gmail.com>
2022-01-25 20:47:47 -0800

    Makes new ctfitem where all it does it do other items display cooldowns

--- src/me/bobthe28th/capturethefart/ctf/items/demo/DemBow.java
+++ src/me/bobthe28th/capturethefart/ctf/items/demo/DemBow.java
@@ -3,10 +3,11 @@ package me.bobthe28th.capturethefart.ctf.items.demo;
 import me.bobthe28th.capturethefart.Main;
 import me.bobthe28th.capturethefart.ctf.CTFPlayer;
 import me.bobthe28th.capturethefart.ctf.itemtypes.CTFItem;
+import me.bobthe28th.capturethefart.ctf.itemtypes.CTFOtherItem;
 import org.bukkit.Material;
 
-public class DemBow extends CTFItem {
-    public DemBow(CTFPlayer player_, Main plugin_) {
-        super("Bazooka", Material.CROSSBOW, 1, player_, plugin_);
+public class DemBow extends CTFOtherItem {
+    public DemBow(CTFItem otherItem_, CTFPlayer player_, Main plugin_) {
+        super("Bazooka", Material.CROSSBOW, otherItem_, 1, player_, plugin_);
     }
 }
---
+++ src/me/bobthe28th/capturethefart/ctf/itemtypes/CTFOtherItem.java
@@ -0,0 +1,21 @@
+package me.bobthe28th.capturethefart.ctf.itemtypes;
+
+import me.bobthe28th.capturethefart.Main;
+import me.bobthe28th.capturethefart.ctf.CTFPlayer;
+import org.bukkit.Material;
+
+public class CTFOtherItem extends CTFItem {
+
+    CTFItem otherCooldown;
+
+    public CTFOtherItem(String itemName_, Material item_, CTFItem otherCooldown_, Integer customModel_, CTFPlayer player_, Main plugin_) {
+        super(itemName_, item_, customModel_, player_, plugin_);
+        otherCooldown=otherCooldown_;
+    }
+
+    @Override
+    public void displayCooldowns() {
+        otherCooldown.displayCooldowns();
+    }
+
+}
