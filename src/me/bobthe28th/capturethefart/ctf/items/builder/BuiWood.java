1b8586c9e01afde09ade397c21eba83300069f26
Bobthe28th <owenmjones28@gmail.com>
2022-02-03 17:07:37 -0800

    armor brrrrrrrrrrrrooooooooooooh

--- src/me/bobthe28th/capturethefart/ctf/items/builder/BuiWood.java
+++ src/me/bobthe28th/capturethefart/ctf/items/builder/BuiWood.java
@@ -12,10 +12,10 @@ public class BuiWood extends CTFBuildUpItem {
     public BuiWood(CTFPlayer player_, Main plugin_, Integer defaultSlot_) {
         super("Wood", Material.OAK_PLANKS, 7, 16, 0, player_, plugin_, defaultSlot_);
         Material tMat = Material.OAK_PLANKS;
-        if (player_.getTeam().getColor() == ChatColor.RED) {
+        if (player_.getTeam().getChatColor() == ChatColor.RED) {
             tMat = Material.CRIMSON_PLANKS;
         } else {
-            if (player_.getTeam().getColor() == ChatColor.BLUE) {
+            if (player_.getTeam().getChatColor() == ChatColor.BLUE) {
                 tMat = Material.WARPED_PLANKS;
             }
         }
